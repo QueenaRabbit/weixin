0058dd3b8d2c5b661a091e2f26beff016885fdb4
QueenaRabbot <624271288@qq.com>
2018-07-29 22:46:39 +0800

    微信

---
+++ wx-test1/pages/index/index.js
@@ -0,0 +1,76 @@
+//index.js
+//获取应用实例
+const app = getApp()
+
+Page({
+  data: {
+    motto: 'index页面数据',
+    userInfo: {},
+    hasUserInfo: false,
+    canIUse: wx.canIUse('button.open-type.getUserInfo')
+  },
+  //事件处理函数
+  bindViewTap: function(ev) {
+    console.log('自定义的事件',ev)
+    wx.navigateTo({
+      url: '../logs/logs'
+    })
+  },
+  onReady() {
+    console.log('onReady');
+  },
+  onShow() {
+    console.log('onShow');
+  },
+  onPullDownRefresh(){
+    console.log('onPullDownRefresh');
+  },
+  onShareAppMessage(){
+    console.log('onShareAppMessage');
+    return {
+      title:'标题!',
+      path:'/pages/api/api'
+    }
+  },
+  onHide() {
+    console.log('onHide');
+  },
+  onLoad: function (options) {
+    console.log('onLoad', options);
+
+    if (app.globalData.userInfo) {
+      this.setData({
+        userInfo: app.globalData.userInfo,
+        hasUserInfo: true
+      })
+    } else if (this.data.canIUse){
+      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
+      // 所以此处加入 callback 以防止这种情况
+      app.userInfoReadyCallback = res => {
+        this.setData({
+          userInfo: res.userInfo,
+          hasUserInfo: true
+        })
+      }
+    } else {
+      // 在没有 open-type=getUserInfo 版本的兼容处理
+      wx.getUserInfo({
+        success: res => {
+          app.globalData.userInfo = res.userInfo
+          this.setData({
+            userInfo: res.userInfo,
+            hasUserInfo: true
+          })
+        }
+      })
+    }
+  },
+  getUserInfo: function(e) {
+    console.log(e)
+    app.globalData.userInfo = e.detail.userInfo
+    this.setData({
+      userInfo: e.detail.userInfo,
+      hasUserInfo: true
+    })
+  }
+})
